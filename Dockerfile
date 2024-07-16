559b8ef2f772f95ef714f552268af9359b22f067
elhilaamine <mohammed-amine.el-hila@polymtl.ca>
2024-07-16 19:59:41 -0400

    tableauDeBord + modification dockerfile page par defaut -> Landing_page

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,26 @@
+#FROM php:8.1-apache
+
+# Install system dependencies
+#RUN apt-get update && apt-get install -y \
+#   libzip-dev \
+#  zip \
+# && docker-php-ext-install zip
+
+# Install PHP extensions
+#RUN docker-php-ext-install pdo pdo_mysql
+
+# Copy custom Apache configuration
+# COPY ./conf/js-no-cache.conf /etc/apache2/conf-available/js-no-cache.conf
+
+# Enable the new configuration
+
+#RUN a2enmod rewrite
+
+# Copy application source
+#COPY . /var/www/html/
+
+#EXPOSE 80
+
 FROM php:8.1-apache
 
 # Install system dependencies
@@ -9,14 +32,25 @@ RUN apt-get update && apt-get install -y \
 # Install PHP extensions
 RUN docker-php-ext-install pdo pdo_mysql
 
-# Copy custom Apache configuration
-# COPY ./conf/js-no-cache.conf /etc/apache2/conf-available/js-no-cache.conf
+# Enable Apache mod_rewrite
+RUN a2enmod rewrite
+
+# Configure Apache to allow .htaccess overrides and specify landing_page.php as DirectoryIndex
+RUN echo "<Directory /var/www/html/> \n\
+    Options Indexes FollowSymLinks \n\
+    AllowOverride All \n\
+    Require all granted \n\
+    DirectoryIndex landing_page.php \n\
+    </Directory>" > /etc/apache2/conf-available/allowoverride.conf
 
 # Enable the new configuration
+RUN a2enconf allowoverride
 
-RUN a2enmod rewrite
+# Set the ServerName to avoid the warning
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
 # Copy application source
 COPY . /var/www/html/
 
+# Expose the port 80 for the web server
 EXPOSE 80
--- dashboard_page.php
+++ dashboard_page.php
@@ -4,8 +4,218 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de bord</title>
+    <link rel="stylesheet" href="dashboard.css">
 </head>
 <body>
-    
+  <div class="wrapper-section">
+    <div class="div-1">
+      <h1 class="heading-3">Tableau de bord - AVA</h1>
+      <div class="div-block-414">
+        <a href="https://webflow.com/website/table-template-81d32b" target="_blank" class="button w-button">Téléverser (.csv)</a>
+      </div>
+    </div>
+    <div class="table-wrapper">
+      <div class="table-row head">
+        <div class="div-block-406"></div>
+        <div class="table-box">
+          <div class="table-heading">Name</div>
+        </div>
+        <div class="table-box">
+          <div class="table-heading">Phone</div>
+        </div>
+        <div class="table-box action">
+          <div class="table-heading">Status</div>
+        </div>
+        <div class="table-box action">
+          <div class="table-heading">Action</div>
+        </div>
+      </div>
+      <div class="table-container">
+        <div class="table-data-wrapper">
+          <div class="scroll-container">
+            <div class="scroll-table-content">
+              <div class="table-row head hide">
+                <div class="div-block-406"></div>
+                <div class="table-box">
+                  <div class="table-heading">Name</div>
+                </div>
+                <div class="table-box">
+                  <div class="table-heading">Email</div>
+                </div>
+                <div class="table-box">
+                  <div class="table-heading">Phone</div>
+                </div>
+                <div class="table-box">
+                  <div class="table-heading">Country</div>
+                </div>
+                <div class="table-box _11">
+                  <div class="table-heading">Last Update</div>
+                </div>
+                <div class="table-box action">
+                  <div class="table-heading">Status</div>
+                </div>
+                <div class="table-box action">
+                  <div class="table-heading">Action</div>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">1</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081b9" class="togglebutton inactive w-inline-block"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081bc" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081be" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">2</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081d5" class="togglebutton active w-inline-block"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081d8" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081da" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">3</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081f1" class="togglebutton w-inline-block"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081f4" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a99081f6" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">4</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a990820d" class="togglebutton active w-inline-block"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908210" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908212" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">5</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908229" class="togglebutton unsure w-inline-block"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a990822c" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a990822e" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">6</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908245" class="togglebutton"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908248" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a990824a" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">7</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908261" class="togglebutton unsure w-inline-block"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908264" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908266" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+              <div class="table-row">
+                <div class="div-block-406 _2">
+                  <div class="table-row-nr">8</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data name">Lamine Yamal</div>
+                </div>
+                <div class="table-box _2">
+                  <div class="table-data">514 889-4581</div>
+                </div>
+                <div class="table-box _2 action">
+                  <a href="#" data-ix="toggle" data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a990827d" class="togglebutton" class ="inactive"></a>
+                </div>
+                <div class="table-box _2 action">
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908280" href="#" class="link-block-12 w-inline-block"><img src="images/crayon_modifier.svg" alt="" class="table-action-icon"></a>
+                  <a data-w-id="ae27a065-dfeb-a9cb-56ca-8b63a9908282" href="#" class="link-block-10 w-inline-block"><img src="images/fermer_X_rouge.svg" alt="" class="table-action-icon-2 x"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="table-details-box">
+        <div class="div-block-412">
+          <div class="div-block-410"><img src="images/fleche_boutton.svg" alt="" class="table-arrows _2"></div>
+          <div class="div-block-411">
+            <div>Page <span class="text-span">1</span> of 1</div>
+          </div>
+          <div class="div-block-410"><img src="images/fleche_boutton.svg" alt="" class="table-arrows"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
