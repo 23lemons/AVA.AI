33ef3d8159f88f7723af1f80c29b4f3b5a549075
elhilaamine <mohammed-amine.el-hila@polymtl.ca>
2024-07-16 08:39:27 -0400

    test_commit_amine

--- dashboard_page.php
+++ dashboard_page.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tableau de bord - AVA.AI</title>
+    <title>Tableau de bord</title>
 </head>
 <body>
     
