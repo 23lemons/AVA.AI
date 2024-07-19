79cd600fec955d3c17857fe084e3cbfebe0f9043
NoahGuy <73375535+NoahGuy@users.noreply.github.com>
2024-07-19 14:58:02 -0400

    modification BD ENCORE

--- AVAdatabase/AvaStructureDB.sql
+++ AVAdatabase/AvaStructureDB.sql
@@ -6,23 +6,29 @@ CREATE TABLE Entreprise (
 );
 
 CREATE TABLE Infos_Entreprise (
-    id_entreprise INT PRIMARY KEY,
+    id_entreprise INT AUTO_INCREMENT PRIMARY KEY,
     nom_entreprise VARCHAR(20) NOT NULL,
     prenom_fondateur VARCHAR(20) NOT NULL,
     nom_fondateur VARCHAR(20) NOT NULL,
     num_tel_entreprise VARCHAR(20) NOT NULL,
     adresse_entreprise TEXT NOT NULL,
-    heures_ouvertures TEXT NOT NULL,
+    heures_ouvertures TEXT,
+    description_entreprise TEXT NOT NULL,
+    description_service TEXT,
+    prix_service DECIMAL(10, 2),
+    temps_livraison VARCHAR(20),
+    FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id_entreprise)
+);
+
+CREATE TABLE Liens_Entreprise (
+    id_entreprise INT AUTO_INCREMENT PRIMARY KEY,
     lien_site_web TEXT NOT NULL,
     lien_page_facebook TEXT,
     lien_page_instagram TEXT,
     lien_page_tiktok TEXT,
     lien_chaine_youtube TEXT,
-    description_entreprise TEXT NOT NULL,
-    description_service TEXT NOT NULL,
-    prix_service DECIMAL(10, 2) NOT NULL,
-    temps_livraison VARCHAR(20) NOT NULL,
     FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id_entreprise)
+
 );
 
 CREATE TABLE Prospects (
